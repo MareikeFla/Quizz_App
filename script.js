3f53e92a5686b01c24df816cb1b41837c5056bf8
MareikeFla <mwidderich@gmail.com>
2023-11-12 22:05:57 +0100

    Add .js file, answer button working, bookmark need to be fixed
    Status: toggle tha class at the bookmark images does not work and I have to figure out why.
    Next Steps: Creating Bookmark-page and Profile Page

---
+++ script.js
@@ -0,0 +1,22 @@
+const cardButton = document.querySelector(".card-button");
+const cardButtonTextShow = document.querySelector(".card-button-textShow");
+const cardButtonTextHide = document.querySelector(".card-button-textHide");
+const cardAnswerText = document.querySelector(".card-answerText");
+const cardAnswerBox = document.querySelector(".card-answerBox");
+
+const cardBookmark = document.querySelector(".card-bookmark");
+const cardBookmarked = document.querySelector(".card-bookmarked");
+const cardNotBookmarked = document.querySelector(".card-notBookmarked");
+
+cardButton.addEventListener("click", () => {
+  cardAnswerText.classList.toggle("card-answerText-hidden");
+  cardAnswerBox.classList.toggle("card-answerBox-active");
+  cardButtonTextShow.classList.toggle("card-button-textHidden");
+  cardButtonTextHide.classList.toggle("card-button-textHidden");
+});
+
+cardBookmark.addEventListener("click", () => {
+  cardBookmarked.classList.toggle("card-bookmark-hide");
+  cardNotBookmarked.classList.toggle("card-bookmark-hide");
+  console.log("click");
+});
