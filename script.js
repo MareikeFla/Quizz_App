64f1b9fcfcc701e5ec921165b951bb9d54723aa6
MareikeFla <mwidderich@gmail.com>
2023-11-15 08:47:29 +0100

    Styled navBar and link boxes with shadows - first approach oj js on navbar when clicking
    What I have done: I styled the nav links and created some icons, but they did not work out, so I added the placeholder star instead. I made a first approach on toggeling the styling class when clicking a link but this is still buggy.
    
    What I learned:
    - When selecting with querySelectorAll you can not just add an event listener (Because it is an array?) but you have to loop over it to use it.
    - Desingning icons is hard - if something looks good in big doesn't guarantee it will look good in tiny also.
    
    Current challenges: Figure out why the eventlistener at the navLinks is not working - maybe switch to buttons?
    
    Next steps: Fixing navbar and dark mode

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ const cardButtonTextHide = document.querySelector(".card-button-textHide");
 const cardAnswerText = document.querySelector(".card-answerText");
 const cardAnswerBox = document.querySelector(".card-answerBox");
 
-const cardBookmark = document.querySelectorAll(".card-bookmark");
+// const cardBookmark = document.querySelectorAll(".card-bookmark");
 const cardBookmarked = document.querySelector(".card-bookmarked");
 const cardNotBookmarked = document.querySelector(".card-notBookmarked");
+
+const footerLinkHome = document.querySelector(".footer-link-home");
+const footerLinkBookmarks = document.querySelector(".footer-link-bookmarks");
+const footerLinkProfile = document.querySelector(".footer-link-profile");
+let footerLinkActive = ".footer-link-home";
+
+const footerLink = document.querySelectorAll(".footer-link");
 
 cardButton.addEventListener("click", () => {
   cardAnswerText.classList.toggle("card-answerText-hidden");
@@ -26,3 +33,25 @@ cardNotBookmarked.addEventListener("click", () => {
   cardNotBookmarked.classList.toggle("card-bookmark-hide");
   console.log("click");
 });
+
+footerLinkHome.addEventListener("click", () => {
+  footerLinkHome.classList.add("footer-activePage");
+  footerLinkActive.classList.remove("footer-activePage");
+  footerLinkActive = ".footer-link-home";
+});
+footerLinkBookmarks.addEventListener("click", () => {
+  footerLinkBookmarks.classList.add("footer-activePage");
+  footerLinkActive.classList.remove("footer-activePage");
+  footerLinkActive = ".footer-link-bookmarks";
+});
+footerLinkProfile.addEventListener("click", () => {
+  footerLinkProfile.classList.add("footer-activePage");
+  footerLinkActive.classList.remove("footer-activePage");
+  footerLinkActive = ".footer-link-profile";
+});
+
+// forEach((footerLink) => {
+//   footerLink.addEventListener("click"), () => {
+//     foo
+//   }
+// };
